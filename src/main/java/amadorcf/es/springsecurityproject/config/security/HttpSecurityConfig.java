4a0bb2da2c93b2438168eeea527f17b84b429ef1
amadorcf <amadorcf@outlook.com>
2024-04-08 13:34:20 +0200

    AUTHORIZE HTTP REQUEST

--- src/main/java/amadorcf/es/springsecurityproject/config/security/HttpSecurityConfig.java
+++ src/main/java/amadorcf/es/springsecurityproject/config/security/HttpSecurityConfig.java
@@ -1,6 +1,7 @@
 package amadorcf.es.springsecurityproject.config.security;
 
 import amadorcf.es.springsecurityproject.config.security.filter.JwtAuthenticationFilter;
+import amadorcf.es.springsecurityproject.persistance.util.RolePermission;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,6 +9,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -26,7 +28,7 @@ public class HttpSecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         SecurityFilterChain filterChain = http
-                // Deshabilitamos csrf ya que se en nuestra apps utilizamos JWTs
+                // Deshabilitamos csrf ya que en la apps se utiliza JWTs
                 .csrf(csrfConfig -> csrfConfig.disable() )
 
                 // Configuracion de sesion sin estado
@@ -36,24 +38,67 @@ public class HttpSecurityConfig {
                 // Configuracion de las estrategias con las que se hara LOGIN
                 .authenticationProvider(daoAuthProvider)
 
-                // Agregamos Filtro antes de que se
+                // Agregamos Filtro
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
 
+                // AUTORIZACION POR PETICIONES HTTP
                 // Configuracion de las rutas publicas
                 .authorizeHttpRequests( authReqConfig ->{
-                    authReqConfig.requestMatchers(HttpMethod.POST, "/customers").permitAll();
-                    authReqConfig.requestMatchers(HttpMethod.POST, "/auth/authenticate").permitAll();
-                    authReqConfig.requestMatchers(HttpMethod.GET, "/auth/validate-token").permitAll();
-                    //authReqConfig.requestMatchers(HttpMethod.POST, "/auth/**").permitAll();
 
-                    // Se pueden crear mas rutas publicas...
-
-                    // RUTAS PRIVADAS: Aseguramos que el usuario tenga que estar autenticado
-                    authReqConfig.anyRequest().authenticated();
+                    buildRequesMatchers(authReqConfig);
                 })
                 .build();
 
         return filterChain;
+    }
+
+    private static void buildRequesMatchers(AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry authReqConfig) {
+        /*Autorizacion de endpoints de productos*/
+        authReqConfig.requestMatchers(HttpMethod.GET, "/products")
+                .hasAuthority(RolePermission.READ_ALL_PRODUCTS.name());
+
+        authReqConfig.requestMatchers(HttpMethod.GET, "/products/{productId}")
+                .hasAuthority(RolePermission.READ_ONE_PRODUCT.name());
+
+        authReqConfig.requestMatchers(HttpMethod.POST, "/products")
+                .hasAuthority(RolePermission.CREATE_ONE_PRODUCT.name());
+
+        authReqConfig.requestMatchers(HttpMethod.PUT, "/products")
+                .hasAuthority(RolePermission.UPDATE_ONE_PRODUCT.name());
+
+        authReqConfig.requestMatchers(HttpMethod.PUT, "/products/{productId}/disabled")
+                .hasAuthority(RolePermission.DISABLE_ONE_PRODUCT.name());
+
+        /*Autorizacion de endspoints de categorias*/
+        authReqConfig.requestMatchers(HttpMethod.GET, "/categories")
+                .hasAuthority(RolePermission.READ_ALL_CATEGORIES.name());
+
+        authReqConfig.requestMatchers(HttpMethod.GET, "/categories/{categoryId}")
+                .hasAuthority(RolePermission.READ_ONE_CATEGORY.name());
+
+        authReqConfig.requestMatchers(HttpMethod.POST, "/categories")
+                .hasAuthority(RolePermission.CREATE_ONE_CATEGORY.name());
+
+        authReqConfig.requestMatchers(HttpMethod.PUT, "/categories")
+                .hasAuthority(RolePermission.UPDATE_ONE_CATEGORY.name());
+
+        authReqConfig.requestMatchers(HttpMethod.PUT, "/categories/{categoryId}/disabled")
+                .hasAuthority(RolePermission.DISABLE_ONE_CATEGORY.name());
+
+        /*Autorizacion de endspoints de mi perfil*/
+        authReqConfig.requestMatchers(HttpMethod.GET, "/auth/profile")
+                .hasAuthority(RolePermission.READ_MY_PROFILE.name());
+
+        /*RUTAS PUBLICAS:*/
+        authReqConfig.requestMatchers(HttpMethod.POST, "/customers").permitAll();
+        authReqConfig.requestMatchers(HttpMethod.POST, "/auth/authenticate").permitAll();
+        authReqConfig.requestMatchers(HttpMethod.GET, "/auth/validate-token").permitAll();
+        //authReqConfig.requestMatchers(HttpMethod.POST, "/auth/**").permitAll();
+
+        // Se pueden crear mas rutas publicas...
+
+        // RUTAS PRIVADAS: Aseguramos que el usuario tenga que estar autenticado
+        authReqConfig.anyRequest().authenticated();
     }
 
 }
