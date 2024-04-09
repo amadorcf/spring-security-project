606fe2ee49719e1adc5ae48021cb8ee87b3807c3
amadorcf <amadorcf@outlook.com>
2024-04-09 09:57:35 +0200

    Created Exception 403 for Access Denied

--- src/main/java/amadorcf/es/springsecurityproject/exception/GlobalExceptionHandler.java
+++ src/main/java/amadorcf/es/springsecurityproject/exception/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import amadorcf.es.springsecurityproject.dto.ApiError;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -27,7 +28,23 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(apiError);
     }
 
-    // Esta excepcion se lanza cuando no se logra hacer el binding del json al obejto Java
+    // Exccepcion 403 para cuando no se tienen los permisos de rol necesarios
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<?> handlerGenericException(AccessDeniedException exception,
+                                                     HttpServletRequest request){
+
+        ApiError apiError = new ApiError();
+        apiError.setBackendMessage(exception.getLocalizedMessage());
+        apiError.setMessage("Access denied. You don not have the necessary permissions to access this feature. Ex" +
+                "Please, contact with the admin if you think that this is an error");
+        apiError.setUrl(request.getRequestURL().toString());
+        apiError.setMethod(request.getMethod());
+        apiError.setTimestamp(LocalDateTime.now());
+
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(apiError);
+    }
+
+    // Esta excepcion se lanza cuando no se logra hacer el binding del json al objeto Java
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<?> handlerMethodArgumentNotValidException(MethodArgumentNotValidException exception,
                                                                     HttpServletRequest request){
